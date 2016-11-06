bff3443d4c798d94e199893cd0d4bd370e6ea252
Kevin McGaire <kevinmcgaire@gmail.com>
2016-11-06 13:24:38 -0500

    Initial Commit

---
+++ rubiks-timer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  rubiks-timer
+//
+//  Created by Kevin McGaire on 2016-11-06.
+//  Copyright © 2016 Kevin McGaire. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ rubiks-timer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  rubiks-timer
+//
+//  Created by Kevin McGaire on 2016-11-06.
+//  Copyright © 2016 Kevin McGaire. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
